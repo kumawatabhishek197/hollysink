95868310cbd1e83d9f44e911d0e2dde4008c7e0f
kumawatabhishek197 <136802082+kumawatabhishek197@users.noreply.github.com>
2023-06-16 23:46:28 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -13,18 +13,17 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 	const fbclid = ctx.query.fbclid;
 
 	// redirect if facebook is the referer or request contains fbclid
-	if (referringURL?.includes('facebook.com') || fbclid) {
+		if (referringURL?.includes('facebook.com') || fbclid) {
 
 		return {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://hollysink.com/` + encodeURI(path as string)
+					`https://hollysink.com` + encodeURI(path as string)
 				}`,
 			},
 		};
 		}
-
 	const query = gql`
 		{
 			post(id: "/${path}/", idType: URI) {
